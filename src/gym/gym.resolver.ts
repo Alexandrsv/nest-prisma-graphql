2caee01a23558f006d866e39be5194f4e2cf44ca
Александр <zbc@mail.ru>
2022-07-04 18:22:05 +0300

    fix @Query import

--- src/gym/gym.resolver.ts
+++ src/gym/gym.resolver.ts
@@ -1,13 +1,12 @@
-import { Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
 import { GymEntity } from './entities/gym.entity';
-import { Query } from '@nestjs/common';
 import { GymService } from './gym.service';
 
 @Resolver(() => GymEntity)
 export class GymResolver {
   constructor(private readonly gymService: GymService) {}
 
-  // @Query(() => [GymEntity])
+  @Query(() => [GymEntity])
   async getAllGyms(): Promise<GymEntity[]> {
     return await this.gymService.getAllGyms();
   }
